cfa057bcf7bfa1a15f2ccbb3928085530116fb64
Astro-hub96 <gtmahesh654@gmail.com>
2025-04-20 16:57:09 +0545

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+"use strict";  // treat JS code as new version
+
+// alert("Hello") we are using node JS not a browser
+
+console.log(3+3); // code readability should be high
+
+
+console.log("nepal")
+
+  console.log(typeof age)
+  console.log(typeof null)
+  console.log(typeof undefined)
