92c21ed2414355709a73598e7f385839535d82b7
Astro-hub96 <gtmahesh654@gmail.com>
2025-04-20 17:37:03 +0545

    initial commit

---
+++ comparision.js
@@ -0,0 +1,20 @@
+// let score = "null"
+
+//  console.log(typeof score)
+//  console.log(typeof score)
+
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log( valueInNumber);
+//  let isLoggedIn  = "Nepal"
+//   let booleanIsLoggedIn =  Boolean(isLoggedIn)
+//    console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 55
+
+ let stringNumber = String(someNumber)
+ console.log(someNumber);
+ console.log(typeof someNumber);
